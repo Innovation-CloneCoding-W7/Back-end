35fc194f3786e3dc8d54523a64cb7b973ab260cc
Cho-El <syzzang21c@gmail.com>
2022-09-09 15:29:47 +0900

    2022-09-09 프로젝트 폴더세팅

---
+++ src/main/java/com/sparta/clonetesla/CloneTeslaApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.clonetesla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneTeslaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneTeslaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/clonetesla/CloneTeslaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.clonetesla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneTeslaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
