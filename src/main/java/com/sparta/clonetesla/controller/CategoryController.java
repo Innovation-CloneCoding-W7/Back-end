2b298078fccb2bedf2db6a8d1299fd16227ea8ab
glooooow <2809yun@gmail.com>
2022-09-13 02:46:11 +0900

    category controller/entity

--- src/main/java/com/sparta/clonetesla/controller/CategoryController.java
+++ src/main/java/com/sparta/clonetesla/controller/CategoryController.java
@@ -1,4 +1,26 @@
 package com.sparta.clonetesla.controller;
 
+import com.sparta.clonetesla.dto.response.ResponseDto;
+import com.sparta.clonetesla.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class CategoryController {
+    private final CategoryService categoryService;
+
+    @RequestMapping(value = "/shop/category", method = RequestMethod.GET)
+    public ResponseDto<?> getCategories() {
+        return categoryService.getCategories();
+    }
+
+//    @RequestMapping(value = "/shop/category/{categoryName}", method = RequestMethod.GET)
+//    public ResponseDto<?> getCategory(@PathVariable String categoryName) {
+//        return categoryService.getCategory(categoryName);
+//    }
+
 }
